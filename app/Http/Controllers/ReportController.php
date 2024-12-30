99a50514d4332e74c21162ff051db0d6743efff2
taufikw27 <taufikwitri@gmail.com>
2024-12-30 20:31:19 +0900

    template exel report ptm done

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -48,10 +48,10 @@ class ReportController extends Controller
     {
         return view('content.report.laporan-stp');
     }
-    public function reportPTM()
-    {
-        return view('content.report.laporan-ptm');
-    }
+    // public function reportPTM()
+    // {
+    //     return view('content.report.laporan-ptm');
+    // }
     public function reportAFP()
     {
         return view('content.report.laporan-afp');
@@ -261,6 +261,241 @@ class ReportController extends Controller
         // Menyimpan file
         $writer = new Xlsx($spreadsheet);
         $fileName = 'Laporan_Kasus_Campak.xlsx';
+        $filePath = storage_path('app/public/' . $fileName);
+
+        $writer->save($filePath);
+        //
+
+        return response()->download($filePath)->deleteFileAfterSend(true);
+    }
+    public function reportPTM()
+    {
+        function calculateAgePTM($dob)
+        {
+            $dob = \Carbon\Carbon::parse($dob);
+            $now = \Carbon\Carbon::now();
+
+            if ($dob->diffInYears($now) > 0) {
+                return $dob->diffInYears($now); // Hitung dalam tahun
+            } elseif ($dob->diffInMonths($now) > 0) {
+                return $dob->diffInMonths($now); // Hitung dalam bulan
+            } else {
+                return $dob->diffInDays($now); // Hitung dalam hari
+            }
+        }
+
+        function calculateAgeUnitPTM($dob)
+        {
+            $dob = \Carbon\Carbon::parse($dob);
+            $now = \Carbon\Carbon::now();
+
+            if ($dob->diffInYears($now) > 0) {
+                return 'years'; // Jika umur dalam tahun
+            } elseif ($dob->diffInMonths($now) > 0) {
+                return 'months'; // Jika umur dalam bulan
+            } else {
+                return 'days'; // Jika umur dalam hari
+            }
+        }
+
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+
+        $sheet->mergeCells('A1:U1');
+        $sheet->setCellValue('A1', 'JUMLAH KASUS DAN KEMATIAN PENYAKIT TIDAK MENULAR MENURUT JENIS KELAMIN DAN UMUR');
+        $sheet
+            ->getStyle('A1')
+            ->getAlignment()
+            ->setHorizontal(Alignment::HORIZONTAL_CENTER);
+        $sheet->getStyle('A1')->getFont()->setBold(true)->setSize(12);
+
+        // $sheet->mergeCells('A2:U2');
+        // $sheet->setCellValue('A2', 'LAPORAN KASUS CAMPAK');
+        // $sheet
+        //     ->getStyle('A2')
+        //     ->getAlignment()
+        //     ->setHorizontal(Alignment::HORIZONTAL_CENTER);
+        // $sheet->getStyle('A2')->getFont()->setBold(true)->setSize(12);
+
+        // $sheet->mergeCells('A3:U3');
+        // $sheet->setCellValue('A3', 'Bulan/Tahun : September / 2024');
+        // $sheet
+        //     ->getStyle('A3')
+        //     ->getAlignment()
+        //     ->setHorizontal(Alignment::HORIZONTAL_CENTER);
+        // $sheet->getStyle('A3')->getFont()->setSize(10);
+
+        // Informasi puskesmas
+        $sheet->setCellValue('A2', 'PDinas Kesehatan Kota Makassar');
+        $sheet->setCellValue('A3', 'Bulan/Tahun : MARET/2024');
+        $sheet->setCellValue('A4', 'Jumlah kasus baru (Kunjungan pertama dan belum tercatat di RS/Fasilitas Kesehatan Lainnya)');
+
+        $headers = [
+            ['JUMLAH KASUS DAN KEMATIAN PENYAKIT TIDAK MENULAR MENURUT JENIS KELAMIN DAN UMUR'],
+            ['Dinas Kesehatan Kota Makassar'],
+            ['Bulan/Tahun : MARET/2024'],
+            ['Jumlah kasus baru (Kunjungan pertama dan belum tercatat di RS/Fasilitas Kesehatan Lainnya)'],
+            ['NO', 'PENYAKIT TIDAK MENULAR', 'Jenis Kelamin dan Umur (Th)', '', '', '', '', '', '', '', 'Jenis Kelamin dan Umur (Th)', '', '', '', '', '', '', '', 'Total'],
+            ['', '', 'Laki-Laki (L)', '', '', '', '', '', '', '', 'Perempuan (P)', '', '', '', '', '', '', '', ''],
+            ['', '', '18-24', '25-34', '35-44', '45-54', '55-64', '65-74', '≥ 75', 'Jumlah', '18-24', '25-34', '35-44', '45-54', '55-64', '65-74', '≥ 75', 'Jumlah', '']
+        ];
+        
+        // Data penyakit berdasarkan gambar
+        $data = [
+            ['1', 'Hipertensi', 1, 4, 3, 5, 6, 7, 21, 47, 13, 23, 25, 31, 27, 17, 113, 182],
+            ['2', 'Diabetes Melitus', 0, 1, 0, 2, 3, 4, 7, 17, 2, 5, 10, 11, 9, 5, 33, 50],
+            ['3', 'Obesitas', 9, 6, 5, 4, 3, 2, 16, 45, 19, 16, 21, 26, 25, 25, 102, 189],
+            ['4', 'Struma', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ['5', 'Tiroksikosis', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ['6', 'Stroke', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ['7', 'Asma', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ['8', 'PPOK', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ['9', 'Osteoporosis', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ['10', 'Penyakit Ginjal Kronik', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ['11', 'Kecelakaan Lalu Lintas Jalan', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ['12', 'Tumor Payudara', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ['13', 'Tumor pada Retina Mata', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ['14', 'Tumor pada Bibir, Rongga Mulut', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ['15', 'Tumor Genitalia Eksterna Pria', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ['16', 'Tumor Genitalia Eksterna Perempuan', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ['17', 'Serviks', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ['18', ' Tumor Genitalia Interna Perempuan (Kecuali Serviks)', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ['', ' Jumlah', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ];
+        
+        // Memasukkan header
+        foreach ($headers as $rowIndex => $headerRow) {
+            $sheet->fromArray($headerRow, null, 'A' . (1 + $rowIndex));
+        }
+        
+        // Memasukkan data penyakit
+        $startRow = count($headers) + 1;
+        $sheet->fromArray($data, null, 'A' . $startRow);
+        
+        
+        // Merge kolom header
+        $sheet->mergeCells('A5:A7'); // NO
+        $sheet->mergeCells('B5:B7'); // PENYAKIT TIDAK MENULAR
+        $sheet->mergeCells('C5:J5'); // Jenis Kelamin dan Umur (Laki-Laki)
+        $sheet->mergeCells('K5:R5'); // Jenis Kelamin dan Umur (Perempuan)
+        $sheet->mergeCells('S5:S7'); // Total
+        $sheet->mergeCells('C6:J6'); // Sub-header Laki-Laki
+        $sheet->mergeCells('K6:R6'); // Sub-header Perempuan
+        
+        // Style header
+        $sheet->getStyle('A1:S7')->getFont()->setBold(true);
+        $sheet->getStyle('A1:S7')
+            ->getAlignment()
+            ->setHorizontal(Alignment::HORIZONTAL_CENTER)
+            ->setVertical(Alignment::VERTICAL_CENTER);
+        $sheet->getStyle('A5:S7')->getFill()
+            ->setFillType(Fill::FILL_SOLID)
+            ->getStartColor()
+            ->setARGB('90EE90'); // Hijau muda
+        // Footer Penjelasan
+        $sheet->setCellValue('B30', 'Mengetahui :');
+        $sheet->mergeCells('B31:G31');
+        $sheet->setCellValue('B31', 'Kepala Puskesmas Tamangapa Makassar,');
+        $sheet->mergeCells('B34:G34');
+        $sheet->setCellValue('B34', 'dr. Hj. Sri Zakiah Usman');
+        $sheet->mergeCells('B35:G35');
+        $sheet->setCellValue('B35', 'NIP: 19700521 20021 2 006');
+
+        // Footer Tanda Tangan
+        $sheet->setCellValue('R30', 'Makassar, Tgl 04 Oktober 2024');
+        $sheet->mergeCells('R31:U31');
+        $sheet->setCellValue('R31', 'Plt.Kepala UPT Puskesmas Tamangapa');
+        $sheet->mergeCells('R34:U34');
+        $sheet->setCellValue('R34', 'dr.Fatimah, M.Kes');
+        $sheet->mergeCells('R35:U35');
+        $sheet->setCellValue('R35', 'NIP. 19851125 201101 2 009');
+
+        // Style Footer
+        $sheet->getStyle('A30:A33')->getFont()->setSize(9); // Font ukuran 9
+        $sheet->getStyle('R30:R35')->getFont()->setSize(9); // Font ukuran 9
+        $sheet
+            ->getStyle('A30:A33')
+            ->getAlignment()
+            ->setHorizontal(Alignment::HORIZONTAL_LEFT);
+        $sheet
+            ->getStyle('R30:R35')
+            ->getAlignment()
+            ->setHorizontal(Alignment::HORIZONTAL_CENTER);
+
+        $actions = Action::select('*') // Pilih semua kolom
+            ->where(function ($query) {
+                $query
+                    ->where('diagnosa', 'like', '%"97"%') // Jika `diagnosa` disimpan dalam format JSON
+                    ->orWhere('diagnosa', '97'); // Jika `diagnosa` disimpan sebagai string
+            })
+            ->get();
+
+        $diagnosisData = [];
+        $data = []; // Inisialisasi array data untuk tabel
+
+        foreach ($actions as $action) {
+            $diagnosisIds = [];
+            if (is_array($action->diagnosa)) {
+                $diagnosisIds = $action->diagnosa;
+            } elseif (is_string($action->diagnosa)) {
+                $decoded = json_decode($action->diagnosa, true);
+                if (is_array($decoded)) {
+                    $diagnosisIds = $decoded;
+                }
+            }
+
+            if (empty($diagnosisIds)) {
+                continue;
+            }
+
+            foreach ($diagnosisIds as $diagnosisId) {
+                if ($diagnosisId != 97) {
+                    // Pastikan hanya mengambil data dengan diagnosa ID 97
+                    continue;
+                }
+
+                $key = $diagnosisId . '-' . ($action->icd10 ?? 'Unknown');
+
+                if (!isset($diagnosisData[$key])) {
+                    $diagnosisData[$key] = [
+                        'name' => Diagnosis::find($diagnosisId)?->name ?? 'Unknown',
+                    ];
+                }
+
+                // Data tabel
+                $data[] = ['', $action->patient->name ? ucwords(strtolower($action->patient->name)) : 'Unknown', '', $action->patient->address ?? 'Unknown', $action->patient->dob && calculateAgeUnit($action->patient->dob) == 'months' ? calculateAge($action->patient->dob) . ' bln' : null, $action->patient->dob && calculateAgeUnit($action->patient->dob) == 'years' ? calculateAge($action->patient->dob) . 'thn' : null, $action->visits ?? '0x', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
+            }
+        }
+        $sheet->fromArray($data, null, 'A11');
+
+        // Penyesuaian lebar kolom secara spesifik
+        $columnWidths = [5, 50, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 15];
+        foreach (range('A', 'S') as $index => $columnID) {
+            $sheet->getColumnDimension($columnID)->setWidth($columnWidths[$index]);
+        }
+        $styleArray = [
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => Border::BORDER_THIN,
+                    'color' => ['argb' => '000000'], // Warna hitam
+                ],
+            ],
+        ];
+
+        // Terapkan border ke seluruh tabel, dari header hingga data terakhir
+        $sheet->getStyle('A5:S26')->applyFromArray($styleArray);
+
+        // Pengaturan kertas dan skala untuk A4
+        $sheet
+            ->getPageSetup()
+            ->setOrientation(PageSetup::ORIENTATION_LANDSCAPE)
+            ->setPaperSize(PageSetup::PAPERSIZE_A4)
+            ->setFitToWidth(1) // Skala agar tabel muat di lebar halaman
+            ->setFitToHeight(0);
+
+        // Menyimpan file
+        $writer = new Xlsx($spreadsheet);
+        $fileName = 'Laporan_PTM.xlsx';
         $filePath = storage_path('app/public/' . $fileName);
 
         $writer->save($filePath);
