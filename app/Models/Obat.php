021ca803a4771bb8a91d4983242d31004f5d6987
zabilsabri <zabilsabrimuhammad0@gmail.com>
2025-06-10 14:37:37 +0800

    `feat`: tambahkan accessor untuk shape obat

--- app/Models/Obat.php
+++ app/Models/Obat.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Obat extends Model
 {
     use HasFactory;
 
-       protected $table = 'obat';
+    protected $table = 'obat';
 
     protected $fillable = [
         'name','code','shape','amount'
@@ -19,4 +20,25 @@ class Obat extends Model
     {
         return $this->hasMany(TerimaObat::class, 'id_obat');
     }
+
+    protected function shapeLabel(): Attribute
+    {
+        return Attribute::get(function () {
+        $shapes = [
+            1 => 'Tablet',
+            2 => 'Botol',
+            3 => 'Pcs',
+            4 => 'Suppositoria',
+            5 => 'Ovula',
+            6 => 'Drop',
+            7 => 'Tube',
+            8 => 'Pot',
+            9 => 'Injeksi',
+        ];
+
+        return $shapes[$this->shape] ?? 'Unknown';
+    });
+
+    }
+
 }
